f9bfd44696bf99152df71ed8ce08c11671569703
José Carlos <josecarlos.alar@gmail.com>
2022-11-08 22:03:15 +0100

    Completed code

--- Biggest_Word/script.js
+++ Biggest_Word/script.js
@@ -1,1 +1,13 @@
-console.log("Hola");
+function biggestWord(phrase) {
+    let word = phrase.split(' ');
+    let palabraMayor = '';
+    for (let words of word) {
+        if (palabraMayor.length == 0) palabraMayor = words;
+        else if (palabraMayor.length < words.length) palabraMayor = words;
+    }
+    console.log(palabraMayor);
+}
+
+// Ejemplo
+console.log(biggestWord("Esta frase puede contener muchas palabras")); // "contener"
+console.log(biggestWord("Ejercicios básicos de JavaScript")); // "Ejercicios"
