8099cf96e663ac5c28900c03ae77669fbefae7fd
josecarlosalar <josecarlos.alar@gmail.com>
2022-11-19 18:37:35 +0100

    Completed Subsets

---
+++ Subsets/script.js
@@ -0,0 +1,25 @@
+/*
+Subsets
+
+Escribe una función que acepte un string como argumento y devuelva todas las 
+partes finales de dicha palabra:
+
+*/
+
+function subsets(word) {
+    let long = word.length;
+    let arr = [];
+    let cadena = '';
+    for (i = 0; i < long - 1; i++) {
+        for (j = i + 1; j < long; j++) {
+            cadena = cadena + word[j];
+        }
+        arr.push(cadena);
+        cadena = '';
+    }
+    return arr;
+
+}
+
+// Ejemplo
+console.log(subsets("message")); // ["essage", "ssage", "sage", "age", "ge", "e"]
