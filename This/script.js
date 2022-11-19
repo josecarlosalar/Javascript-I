56ef6ebc4763d7d1d78d2ba581d54510039a1de9
josecarlosalar <josecarlos.alar@gmail.com>
2022-11-19 18:53:04 +0100

    Completed This

---
+++ This/script.js
@@ -0,0 +1,27 @@
+/*
+This
+
+¿Cual es la salida de los logs en el siguiente código? 
+Intenta razonar, no te limites a ejecutar la solución.
+
+
+ */
+
+
+var surname = "Pérez";
+var person = {
+    name: "Juan",
+    surname: "González",
+    wife: {
+        name: "Ana",
+        surname: "Jiménez",
+        getSurname: function() {
+            return this.surname;
+        },
+    },
+};
+
+console.log(person.wife.getSurname()); // Retorna "Jiménez" porque quien invoca la función es 'wife' el cual dispone de una variable 'surname = Jiménez' 
+var surnameFunction = person.wife.getSurname;
+console.log(surnameFunction()); // Retorna "Pérez" porque surnameFunction" está declarada con 'var' y esto hace que sea una variable de ambito global por lo que el objeto 'this' en este caso es el objeto global ya que es quien invoca la función 'getSurname' 
+console.log(surnameFunction.call(person)); // Retorna "González porque en este caso 'surnameFuction' está forzando a que quien invoca la función sea el objeto 'person' el cuen contiene una variable 'surname = González'
