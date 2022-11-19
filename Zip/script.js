8a531aefe843e4b0202515367fe6f8b2143d4c01
josecarlosalar <josecarlos.alar@gmail.com>
2022-11-19 19:18:14 +0100

    Completed Zip

---
+++ Zip/script.js
@@ -0,0 +1,21 @@
+/*
+
+Zip
+
+Crea una función zipObject tal que acepte un array de claves como primer argumento 
+y un array de valores como segundo argumento y cuyo objetivo sea crear un objeto uniendo 
+las claves con sus valores. Asumir que el array de claves tiene como mínimo la misma longitud 
+que el de valores:
+
+*/
+
+function zipObject(keys, values) {
+    let zipObj = {};
+    for (i = 0; i < keys.length; i++) {
+        zipObj[keys[i]] = values[i];
+    }
+    return zipObj;
+}
+
+// Ejemplo
+console.log(zipObject(["spanish", "english", "french"], ["hola", "hi", "salut"])); // {spanish: "hola", english: "hi", french: "salut"}
